0ac1eb07baeb113d31cc4abcc6b43f8b67ae677f
Michael Mottola <mottola64@gmail.com>
2019-02-20 03:41:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,6 +6,23 @@ def array_with_two_elements
   [1, 2]
 end
 
-def first_element
+def first_element(Array)
+  Array[0]
+end
+
+def third_element(Array)
+  Array[2]
+end
+
+def last_element(Array)
+  Array[3]
+end
+
+def first_element_with_array_methods
   Array.first
 end
+
+def last_element_with_array_methods
+  Array.last
+end
+
